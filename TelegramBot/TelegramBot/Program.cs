6c63da0d1690dac3fa61332cf385482411cbbabe
IlinArtem78 <artem.ilin1994@gmail.com>
2024-08-07 13:52:22 +0300

    Модуль 11. Задание 11.2.4

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text;
+using Telegram.Bot;
+
+
+
+namespace VoiceTexterBot
+{
+   
+
+
+}    
+
---
+++ TelegramBot/TelegramVoiceBot/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using Telegram.Bot;
+
+
+
+namespace TelegramVoiceBot
+{
+
+    class Program
+    {
+
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("7408197128:AAFtQ7bmx-G_F5GvBpn_XGAelq0Svqncs8g"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
